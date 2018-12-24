ae65b16fb7a9ef6e11f78613262083d37e654a42
CClannad <834594158@qq.com>
2018-12-24 20:19:27 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/example/zc/coolweather/MainActivity.java
+++ app/src/main/java/com/example/zc/coolweather/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.zc.coolweather;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
